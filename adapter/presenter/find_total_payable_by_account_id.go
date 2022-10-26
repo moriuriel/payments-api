d9225e0e3b38187fb9797e7522c222c01f7a1a12
Uriel Mori <moriuriel@gmail.com>
2022-10-25 21:52:41 -0300

    feat: create func toFixed and round

--- adapter/presenter/find_total_payable_by_account_id.go
+++ adapter/presenter/find_total_payable_by_account_id.go
@@ -1,6 +1,8 @@
 package presenter
 
 import (
+	"math"
+
 	"github.com/moriuriel/go-payments/usecase"
 )
 
@@ -12,7 +14,16 @@ func NewFindTotalPayableByAccountIDPresenter() usecase.FindTotalPayableByAccount
 
 func (c findPayableByAccountIDPresenter) Output(totalPaid float64, totalWaitingFunds float64) usecase.FindTotalPayableByAccountIDOutput {
 	return usecase.FindTotalPayableByAccountIDOutput{
-		TotalPaid:         totalPaid,
-		TotalWaitingFunds: totalWaitingFunds,
+		TotalPaid:         toFixed(totalPaid, 2),
+		TotalWaitingFunds: toFixed(totalWaitingFunds, 2),
 	}
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
 }
