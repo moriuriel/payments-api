0d841879b9ab7a7cb8ec386971c40efb9537f5ea
Uriel Mori <moriuriel@gmail.com>
2022-11-08 20:53:31 -0300

    feat: create response struct

--- adapter/api/response/success.go
+++ adapter/api/response/success.go
@@ -3,12 +3,20 @@ package response
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 )
 
-type Success struct {
-	statusCode int
-	result     interface{}
-}
+type (
+	Success struct {
+		statusCode int
+		result     interface{}
+	}
+	Response struct {
+		Content    interface{} `json:"content"`
+		StatusCode int         `json:"statusCode"`
+		Timestamp  string      `json:"timestamp"`
+	}
+)
 
 func NewSuccess(result interface{}, status int) Success {
 	return Success{
@@ -20,5 +28,12 @@ func NewSuccess(result interface{}, status int) Success {
 func (r Success) Send(w http.ResponseWriter) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(r.statusCode)
-	return json.NewEncoder(w).Encode(r.result)
+
+	response := Response{
+		StatusCode: r.statusCode,
+		Timestamp:  time.Now().Format(time.RFC3339),
+		Content:    r.result,
+	}
+
+	return json.NewEncoder(w).Encode(response)
 }
